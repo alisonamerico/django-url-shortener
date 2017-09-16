c2881a761b095ec16b43c3fdcce3adc05ee2b4a1
alisonamerico <alison.americo@gmail.com>
2017-09-15 21:21:31 -0300

    inserting urls login, logout, signup

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,18 @@
 from django.conf.urls import include, url
 from core.views import *
+from django.contrib.auth import views as auth_views
+from core import views as core_views
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
-    url(r'^$', index, name='index'),
+    url(r'^$', core_views.index, name='index'),
     # for our home/index page
-
-    url(r'^register$', register, name='register'),
-    # for our register page
-
-    #url(r'^login$', login, name='login'),
-    # for our login page
-
-    #url(r'^logout$', logout, name='logout'),
-    # for our logout page
-
-    url(r'^short_id/$', redirect_original, name='redirectoriginal'),
+    url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^signup/$', core_views.signup, name='signup'),
+    url(r'^(?P<short_id>\w{6})$', redirect_original, name='redirectoriginal'),
     # when short URL is requested it redirects to original URL
-
     url(r'^makeshort/$', shorten_url, name='shortenurl'),
     # this will create a URL's short id and return the short URL
     ]
