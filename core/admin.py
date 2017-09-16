9df9f94f242fbca40a75b6c854e56d37aa3d790c
alisonamerico <alison.americo@gmail.com>
2017-09-15 21:44:02 -0300

    configuring fields in admin

--- core/admin.py
+++ core/admin.py
@@ -3,17 +3,16 @@ from core.models import Urls
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
-class UrlsAdmin(admin.ModelAdmin):
-    list_display = ('short_id','httpurl','pub_date', 'count')
-    ordering = ('-pub_date',)
 
 class UrlsResource(resources.ModelResource):
-
     class Meta:
         model = Urls
-        verbose_name_plural = "Urls"
 
 class UrlsAdmin(ImportExportModelAdmin):
     resource_class = UrlsResource
+    list_display = ('short_id','httpurl','pub_date', 'count')
+    search_fields = ('short_id', 'httpurl', 'pub_date')
+    ordering = ('-pub_date',)
+
 
 admin.site.register(Urls, UrlsAdmin)
