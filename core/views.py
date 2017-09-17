01132785398dc632fb1822f8adf186a6bd42774e
alisonamerico <alison.americo@gmail.com>
2017-09-17 12:08:04 -0300

    updating urls

--- core/views.py
+++ core/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 from core.forms import SignUpForm
 
 
-@login_required
+#@login_required
 def index(request):
     return render(request, 'index.html')
 
