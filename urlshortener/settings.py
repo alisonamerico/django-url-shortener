69338007d9a08c97b7ff0573590f405c5244aab1
alisonamerico <alison.americo@gmail.com>
2017-09-22 19:02:53 -0300

    configuring ALLOWED_HOSTS

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from decouple import config
+from decouple import config, Csv
 from dj_database_url import parse as dburl
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
 
 
 # Application definition
@@ -134,5 +134,3 @@ LOGIN_REDIRECT_URL = 'core:index'
 
 
 #Default settings URL
-
-SITE_URL = "http://localhost:8000"
